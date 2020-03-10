4e7e5d83fc5843ae4da1c9772dba1a38646fe73b
lukas_000 <zaborowski.luk@gmail.com>
2020-03-10 17:54:11 +0100

    first

---
+++ src/test/java/pl/zaborowski/videoapp/VideoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zaborowski.videoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
